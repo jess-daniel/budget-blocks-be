e96534fbc7aaa02dd7e3318f293e1f31db193c50
Kyle Corbin <kylecorbin26@gmail.com>
2020-02-04 13:25:31 -0500

    working on the post->get conversino

--- server/plaid/getAccessToken-middleware.js
+++ server/plaid/getAccessToken-middleware.js
@@ -9,6 +9,7 @@ module.exports = (req, res, next) => {
     .then(access => {
       if (access) {
         req.body.access = access.access_token;
+        console.log('MIDDLEWARE PASSED', req.body)
         next();
       } else {
         res
