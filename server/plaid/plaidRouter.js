35082a813d88c540f1bdb3cc4423ad1c02e05aaf
Kyle Corbin <kylecorbin26@gmail.com>
2020-02-06 17:51:32 -0500

    last test before implementation of automatic updates for transactions

--- server/plaid/plaidRouter.js
+++ server/plaid/plaidRouter.js
@@ -114,7 +114,7 @@ router.post('/webhook', async (req,res)=>{
       console.log("DATE RANGE FOR TRANSACTIONS",start, end)
       
       //This is us getting the transactions 
-      const {transactions} = await client.getTransactions(access_token, start, end,{count:7});
+      const {transactions} = await client.getTransactions(access_token, start, end,{count:1});
 
       //This is a more refined version of what I had before on line 54. 
      const done = await qs.WEB_insert_transactions(transactions, userID.id)
