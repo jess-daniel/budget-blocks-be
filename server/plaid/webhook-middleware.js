8e09fa56356e02754dd84b67b25393b60dcf60cc
Kyle Corbin <kylecorbin26@gmail.com>
2020-02-06 19:07:08 -0500

    Final test, if it works then the middleware will be used for most of these webhook conditionals

--- server/plaid/webhook-middleware.js
+++ server/plaid/webhook-middleware.js
@@ -14,12 +14,21 @@ module.exports = async(req,res,next)=>{
         //This is getting the userID for later. We need to get the userID from the item_id plaid gave us to get the EXACT user
         const userID = await qs.WEB_get_userID(item_id)
 
+              //this is the current day
+      var end = (new Date()).toISOString().replace(/-/g, '-').split('T')[0]
+      //all three of these are to get 30 days behind the current day
+      var today = new Date()
+      var start = new Date().setDate(today.getDate()-30)
+      var start = (new Date(start)).toISOString().replace(/-/g, '-').split('T')[0]
+
         if(!item_id || !pgItemId || !userID){
             console.log('WEB-MIDLEWARE ERROR')
         }else{
             body.item_id = item_id
             body.pgItemId = pgItemId
             body.userID = userID
+            body.start = start
+            body.end = end
             next();
         }
 
