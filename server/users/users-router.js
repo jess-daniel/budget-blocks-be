84254842e3728ac3ab1dc757c5d12bc64ac2aea0
Kyle Corbin <kylecorbin26@gmail.com>
2020-02-13 19:27:58 -0500

    Fixing the return value

--- server/users/users-router.js
+++ server/users/users-router.js
@@ -83,7 +83,7 @@ router.put("/categories/:userId", userExists, paramCheck.idAndBody, async (req, 
   try {
     const update = await Users.editUserCategoryBudget(id,body.categoryid,body.budget);
     console.log("THE RESULT OF THE UPDATE", update)
-    if (update == 1) {
+    if (update) {
       res.status(202).json({
         userid: id,
         categoryid: body.categoryid,
