fad54890dda3e6d2b5dd093fe38b61155658a964
Kyle Corbin <kylecorbin26@gmail.com>
2020-02-13 20:09:24 -0500

    Bug came back up, testing another solution

--- server/plaid/getAccessToken-middleware.js
+++ server/plaid/getAccessToken-middleware.js
@@ -7,22 +7,23 @@ module.exports = (req, res, next) => {
 
   if(!id){
     res.status(400).json({message:'please add a param to the end of the endpoint'})
+  }else{
+    console.log("ID THAT SHOULD FAIL", id)
+    qs.getAccessToken(id)
+      .then(access => {
+        if (access) {
+          req.body.access = access.access_token;
+          next();
+        } else {
+          res
+            .status(404)
+            .json({message: 'No access_Token found for that user id provided'});
+        }
+      })
+  
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({message: 'big error on server brother'});
+      });
   }
-
-  qs.getAccessToken(id)
-    .then(access => {
-      if (access) {
-        req.body.access = access.access_token;
-        next();
-      } else {
-        res
-          .status(404)
-          .json({message: 'No access_Token found for that user id provided'});
-      }
-    })
-
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({message: 'big error on server brother'});
-    });
 };
