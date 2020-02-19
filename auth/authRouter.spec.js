dc0b030410c1722950cc0ba221c214f36d95604a
Anthony Sutton <anthony.sutton95@gmail.com>
2020-02-19 12:29:10 -0800

    Updated coverage report and added better testing coverage for the server

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -3,33 +3,69 @@ const server = require("../data/server");
 const Users = require("../users/users-model");
 const db = require("../data/db-config");
 
-describe("Users Login & Register", function() {
+describe("Users Login & Register", () => {
   // Clears the users table when a test is ran to ensure that the db is clean
-  beforeEach(async () => {
+  afterAll(async () => {
     await db("users").truncate();
   });
 
-  it("Should allow a new user to be created", async function() {
-    // Registers a new user
-    try {
-      await Users.addUser({ email: "test@test.com", password: "password" });
-      const response = await request(server).post("/register");
+  afterAll(done => {
+    return server && server.close(done);
+  });
 
+  describe("Logs user in", () => {
+    beforeAll(async () => {
+      await db("users").truncate();
+    });
+    test("Should allow a new user to be created", async () => {
+      // Registers a new user
+
+      const testing = await Users.addUser({
+        email: "test@test.com",
+        password: "password"
+      });
+
+      const response = await request(server).post("/register");
       // Checks the DB if that newly created yser exists
       const users = await db("users");
       expect(users).toHaveLength(1);
-    } catch (error) {
-      console.log(error);
-    }
+    });
   });
 
-  // describe("Registration", async function() {
-  //   it("Should allow a user to login", async function() {
-  //     const response = await Users.login({
-  //       email: "test@test.com",
-  //       password: "password"
-  //     });
-  //     expect(response.status).toBe(200);
-  //   });
-  // });
+  test("Should allow a user to login", async () => {
+    const user = { email: "test@test.com", password: "password" };
+
+    // await Users.login(user);
+    const response = await request(server)
+      .post("/api/auth/login")
+      .send(user);
+    // console.log(response);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeDefined();
+  });
+
+  test("Should return an error if no body is passed in", async () => {
+    const response = await request(server).post("/api/auth/login");
+
+    expect(response.body.error).toBe(
+      "No information was passed into the body."
+    );
+  });
+
+  test("Should return an error if the user does not exist", async () => {
+    const user = { email: "fake@test.com", password: "password" };
+
+    // await Users.login(user);
+    const response = await request(server)
+      .post("/api/auth/login")
+      .send(user);
+    // console.log(response);
+
+    expect(response.status).toBe(500);
+    expect(response.status).toBeDefined();
+    expect(response.body.message).toBe(
+      "Unable to find the user by the email provided "
+    );
+  });
 });
--- users/usersModel.spec.js
+++ users/usersModel.spec.js
@@ -9,43 +9,27 @@ describe("Users Model", function() {
     return server && server.close(done);
   });
 
-  it("GET /users", async function() {
-    try {
-      const response = await request(server).get("/");
-      expect(response.status).toBe(200);
-    } catch (error) {
-      console.log(error);
-    }
+  it("GET /users", async () => {
+    const response = await request(server).get("/");
+    expect(response.status).toBe(200);
   });
 
   // Ensures that the response is a json object
 
-  it("Should return a JSON object", async function() {
-    try {
-      const response = await request(server).get("/");
-      expect(response.type).toMatch(/json/i);
-    } catch (error) {
-      console.log(error);
-    }
+  it("Should return a JSON object", async () => {
+    const response = await request(server).get("/");
+    expect(response.type).toMatch(/json/i);
   });
-});
 
-// Checks if the response of categories of user id 1 to not be 0
-it("GET /categories/:userId", async function() {
-  try {
+  // Checks if the response of categories of user id 1 to not be 0
+  it("GET /categories/:userId", async () => {
     const response = await request(server).get("/categories/1");
     expect(response.status).toBe(200);
-  } catch (error) {
-    console.log(error);
-  }
-});
+  });
 
-// // Checks that the response to the categories is a json object
-it("Should return a JSON object", async function() {
-  try {
+  // // Checks that the response to the categories is a json object
+  it("Should return a JSON object", async () => {
     const response = await request(server).get("/categories/1");
     expect(response.type).toMatch(/json/i);
-  } catch (error) {
-    console.log(error);
-  }
+  });
 });
