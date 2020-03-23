9fd301fdb79463ce4d222db438b2b675e7f5c153
Justin Price <mr.price@Justins-iMac.local>
2020-03-23 14:04:44 -0400

    sentry

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,90 @@
-// Update with your config settings.
+// // Update with your config settings.
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/users.db3"
-    },
-    seeds: {
-      directory: "./data/seeds"
-    },
-    migrations: {
-      directory: "./data/migrations"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    }
+//   development: {
+//     client: "sqlite3",
+//     useNullAsDefault: true,
+//     connection: {
+//       filename: "./data/users.db3"
+//     },
+//     seeds: {
+//       directory: "./data/seeds"
+//     },
+//     migrations: {
+//       directory: "./data/migrations"
+//     },
+//     pool: {
+//       afterCreate: (conn, done) => {
+//         conn.run("PRAGMA foreign_keys = ON", done);
+//       }
+//     }
+//   },
+//   production: {
+//     client: "pg",
+//     connection: process.env.DATABASE_URL,
+//     migrations:{
+//       directory: "./data/migrations"
+//     },
+//     seeds:{
+//       directory:"./data/seeds"
+//     }
+//   }
+// development: {
+
+// client: "sqlite3",
+
+// useNullAsDefault: true,
+
+// connection: {
+
+// filename: "./data/users.db3"
+
+// },
+
+// seeds: {
+
+// directory: "./data/seeds"
+
+// },
+
+// migrations: {
+
+// directory: "./data/migrations"
+
+// },
+
+// pool: {
+
+// afterCreate: (conn, done) => {
+
+// conn.run("PRAGMA foreign_keys = ON", done);
+
+// }
+
+// }
+
+// },
+
+development: {
+
+  client: "pg",
+  
+  connection: process.env.DEV_DATABASE_URL,
+  
+  migrations: {
+  
+  directory: "./data/migrations"
+  
   },
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    migrations:{
-      directory: "./data/migrations"
-    },
-    seeds:{
-      directory:"./data/seeds"
-    }
+  
+  seeds: {
+  
+  directory: "./data/seeds"
+  
   }
+  
+  },
 };
+
+
