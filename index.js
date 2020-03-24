ba1ca45549186c3ea873b9ce817953298542aaf4
Justin Price <mr.price@Justins-iMac.local>
2020-03-24 13:26:27 -0400

    moved sentry init into serverjs, moved creds into env file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const server = require("./data/server.js");
 const PORT = process.env.PORT || 5000;
-const Sentry = require('@sentry/node');
-Sentry.init({ dsn: 'https://c0e164d7a07b4f54854d29fc3c62f50f@sentry.io/5167996' });
 
 server.listen(PORT, () => {
   console.log(`\n*** Server running at http://locahost:${PORT}... ***\n`);
