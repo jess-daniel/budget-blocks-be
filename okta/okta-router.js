63f118b933ea9ee655c1b684f02add948a865ebe
Edward Blanciak <blanciak.edward2@gmail.com>
2020-06-11 16:09:00 -0400

    adds tests for post and delete okta routes

--- okta/okta-router.js
+++ okta/okta-router.js
@@ -3,6 +3,8 @@ const dataBase = require('./okta-model');
 
 // SECTION Middleware
 const requireAuthentication = require('./middleware/require_authentication');
+const { response } = require('express');
+const { route } = require('../plaid/plaidRouter');
 
 // SECTION GET -- Get All Users
 router.get('/users', requireAuthentication, (req, res) => {
@@ -26,24 +28,27 @@ router.post('/users', requireAuthentication, (req, res) => {
     name: oktaInfo.name,
     email: oktaInfo.email,
   };
-
+  // console.log('email: ', userInfo.email);
+  // console.log('name: ', userInfo.name);
   // const email = userInfo.email;
 
   dataBase
     .findUserByEmail(userInfo.email)
     .then((user) => {
-      if (user) {
-        res.status(200).json({ data: user });
-      } else {
+      if (user === undefined) {
         dataBase
           .addUser(userInfo)
           .then((user) => {
-            res.status(201).json({ data: user });
+            res.status(201).json({ data: user, message: 'User created.' });
           })
           .catch((err) => {
             res.status(500).json({ error: err.message });
             console.log(err.message);
           });
+      } else {
+        res
+          .status(200)
+          .json({ data: user, message: 'User already exists.' });
       }
     })
     .catch((err) => {
@@ -81,4 +86,73 @@ router.put('/users/:userId', requireAuthentication, (req, res) => {
     });
 });
 
+///////////////////////////////////////////
+///////////TEST ENDPOINTS//////////////////
+// router.get('/users/:email', (req, res) => {
+//   const email = req.params.email;
+
+//   dataBase
+//     .findUserByEmail(email)
+//     .then((response) => {
+//       res.status(200).json({ data: response });
+//       console.log(response);
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ error: err.message });
+//     });
+// });
+
+// router.post('/users/test', (req, res) => {
+//   const userInfo = req.body;
+
+//   dataBase
+//     .addUser(userInfo)
+//     .then((response) => {
+//       res.status(201).json({ data: response });
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ error: err.message });
+//     });
+// });
+
 module.exports = router;
+
+// router.post('/users', requireAuthentication, (req, res) => {
+//   const oktaInfo = req.body;
+//   const userInfo = {
+//     name: oktaInfo.name,
+//     email: oktaInfo.email,
+//   };
+//   console.log('email: ', userInfo.email);
+//   console.log('name: ', userInfo.name);
+//   // const email = userInfo.email;
+
+//   dataBase
+//     .findUserByEmail(userInfo.email)
+//     .then((user) => {
+//       if(user === undefined){
+
+//       }
+//       console.log('findBy:', user);
+//       if (Object.keys(user).length > 0) {
+//         res
+//           .status(200)
+//           .json({ data: user, message: 'User already exists.' });
+//       } else {
+//         dataBase
+//           .addUser(userInfo)
+//           .then((user) => {
+//             console.log('add:', user);
+//             res.status(201).json({ data: user, message: 'User created.' });
+//           })
+//           .catch((err) => {
+//             res.status(500).json({ error: err.message });
+//             console.log(err.message);
+//           });
+//       }
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//       res.status(500).json({ error: err.message });
+//     });
+// });
--- okta/okta-router.spec.js
+++ okta/okta-router.spec.js
@@ -1,44 +1,145 @@
-const request = require("supertest");
-const server = require("../server");
-const db = require("../data/db-config");
+const request = require('supertest');
+const server = require('../server');
+const db = require('../data/db-config');
 
-describe("OKTA ROUTER", () => {
-  describe("GET /users - all users", () => {
-    it("should return 200", () => {
+describe('OKTA ROUTER', () => {
+  // NOTE This token will need updated in order for this test to work.
+  const accessToken = process.env.TEST_ACCESS_TOKEN;
+  // console.log(accessToken);
+
+  describe('GET /users - all users', () => {
+    it('should return 200', () => {
       return request(server)
-        .get("/api/users")
+        .get('/api/users')
+        .set('Authorization', `Bearer ${accessToken}`)
         .then((res) => {
           expect(res.status).toBe(200);
         });
     });
 
-    it("returns an array of objects of users", () => {
+    it('should return 401 with no access token', () => {
+      return request(server)
+        .get('/api/users')
+        .then((res) => {
+          expect(res.status).toBe(401);
+        });
+    });
+
+    it('returns an array of objects of users', () => {
+      // NOTE expected needs to be updated as the data changes in the seeds.
       const expected = [
         {
           id: 1,
-          name: "Edward Blanciak",
-          email: "budgetblocks@gmail.com",
-        },
-        {
-          id: 2,
-          name: "Joe Smith",
-          email: "JoeSmith@gmail.com",
+          name: 'Edward Blanciak',
+          email: 'budgetblocks@gmail.com',
+          onboarding_complete: false,
+          state: null,
+          city: null,
         },
       ];
 
       return request(server)
-        .get("/api/users/")
+        .get('/api/users/')
+        .set('Authorization', `Bearer ${accessToken}`)
         .then((res) => {
-          console.log("res", res.body);
           expect(res.body.data).toEqual(expected);
-        })
-        .catch((err) => {
-          console.log(err.message);
         });
     });
   });
 
-  it("POST /users - add user", () => {});
+  describe('POST /users - add user', () => {
+    const newUser = {
+      name: 'Joe Smith',
+      email: 'Joe@gmail.com',
+    };
 
-  it("DELETE /users - delete user", () => {});
+    const existingUser = {
+      name: 'Edward Blanciak',
+      email: 'budgetblocks@gmail.com',
+    };
+
+    const deleteUser = () => {
+      return db('users').where({ name: newUser.name }).del();
+    };
+
+    afterEach(deleteUser);
+
+    it('it should return 201', () => {
+      return request(server)
+        .post('/api/users')
+        .send(newUser)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .then((res) => {
+          expect(res.status).toBe(201);
+        });
+    });
+
+    it('it should return Message: User created.', () => {
+      return request(server)
+        .post('/api/users')
+        .send(newUser)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .then((res) => {
+          expect(res.body.message).toEqual('User created.');
+        });
+    });
+
+    it('it should return 200', () => {
+      return request(server)
+        .post('/api/users')
+        .send(existingUser)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .then((res) => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    it('it should return Message: User already exists.', () => {
+      return request(server)
+        .post('/api/users')
+        .send(existingUser)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .then((res) => {
+          expect(res.body.message).toEqual('User already exists.');
+        });
+    });
+  });
+
+  describe('DELETE /users/:userId - delete user', () => {
+    const newUser = {
+      name: 'Joe Smith',
+      email: 'Joe@gmail.com',
+    };
+
+    const addUser = () => {
+      return request(server)
+        .post('/api/users')
+        .send(newUser)
+        .set('Authorization', `Bearer ${accessToken}`);
+    };
+
+    beforeEach(addUser);
+
+    it('it should return 200', () => {
+      return request(server)
+        .delete('/api/users')
+        .send(newUser)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .then((res) => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    it('it should return Message: User successfully deleted!', () => {
+      return request(server)
+        .delete('/api/users')
+        .send(newUser)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .then((res) => {
+          expect(res.body.message).toEqual('User successfully deleted!');
+        });
+    });
+  });
+
+  describe('PUT /users/:userId - Update User', () => {});
 });
